0b70fe99081c78f72f18664370ea91c7085eca07
Trieu Tien Quynh <160729262+tienquynh1312@users.noreply.github.com>
2026-04-15 17:07:11 +0700

    Clean

--- NumberUtilsStatementTest.java
+++ NumberUtilsStatementTest.java
@@ -7,15 +7,10 @@ public class NumberUtilsStatementTest {
     public void testStatementCoverage() {
         NumberUtils utils = new NumberUtils();
         
-        // Ca kiểm thử 1: Có số dương. 
-        // Bao phủ: Khởi tạo for, điều kiện for, điều kiện if (True), lệnh return trong if.
         assertEquals(5, utils.findFirstPositive(new int[]{5}));
-        
-        // Ca kiểm thử 2: Không có số dương. 
-        // Bao phủ: Khởi tạo for, điều kiện for, điều kiện if (False), chạy hết for, lệnh return -1 ở cuối.
+       
         assertEquals(-1, utils.findFirstPositive(new int[]{-2}));
         
-        // Ca kiểm thử 3: Mảng rỗng
         assertEquals(-1, utils.findFirstPositive(new int[]{}));
     }
 }
