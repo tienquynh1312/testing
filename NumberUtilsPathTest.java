0764595c10794d4608dfbfc0161037713ede6a7e
tienquynh1312 <tquynhh1312@gmail.com>
2026-04-15 17:00:19 +0700

    Remove notes from NumberUtilsPathTest.java

---
+++ NumberUtilsPathTest.java
@@ -0,0 +1,15 @@
+﻿import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class NumberUtilsPathTest {
+
+    @Test
+    public void testPathCoverage() {
+        NumberUtils utils = new NumberUtils();
+        
+        assertEquals(-1, utils.findFirstPositive(new int[]{}));
+        assertEquals(-1, utils.findFirstPositive(new int[]{-1, -2}));
+        assertEquals(10, utils.findFirstPositive(new int[]{10, -5}));
+        assertEquals(7, utils.findFirstPositive(new int[]{-3, 7, -1}));
+    }
+}
