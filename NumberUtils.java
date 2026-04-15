ac2e4e118311523751f87192163a555bc1a0a818
tienquynh1312 <tquynhh1312@gmail.com>
2026-04-15 17:00:19 +0700

    Remove notes from NumberUtils.java

--- NumberUtils.java
+++ NumberUtils.java
@@ -1,7 +1,7 @@
 public class NumberUtils {
     public int findFirstPositive(int[] numbers) {
-        for (int i = 0; i < numbers.length; i++) { // Vòng lặp
-            if (numbers[i] > 0) {                  // Lệnh rẽ nhánh
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > 0) {
                 return numbers[i];
             }
         }
