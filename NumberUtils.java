be5d34d7711cc70678a262c73e07237c2912d5e3
tienquynh1312 <tquynhh1312@gmail.com>
2026-04-15 16:30:54 +0700

    Update NumberUtils.java

--- NumberUtils.java
+++ NumberUtils.java
@@ -1,3 +1,10 @@
 public class NumberUtils {
-    
+    public int findFirstPositive(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) { // Vòng lặp
+            if (numbers[i] > 0) {                  // Lệnh rẽ nhánh
+                return numbers[i];
+            }
+        }
+        return -1;
+    }
 }
